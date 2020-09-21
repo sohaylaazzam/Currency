f48fe305e04bc0139158329b02db7ca89011238c
sohayla ashraf <lollycaty2@hotmail.com>
2020-09-21 14:52:10 +0200

    First Commit

---
+++ currency.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float USDP=0.063,USD,Result;
+    printf("Enter the amount=");
+    scanf("%f",&USD);
+
+    Result=USDP*USD;
+    printf("amount= %f LE", Result);
+
+
+}
+
